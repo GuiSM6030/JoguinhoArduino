04942a858220cbe293b1560615144efeb2707694
Natashinha98 <147116370+Natashinha98@users.noreply.github.com>
2025-03-25 20:47:23 -0300

    tocar nota adicionado

--- primeira.cpp
+++ primeira.cpp
@@ -12,3 +12,9 @@ LiquidCrystal lcd(9, 11, 10, 12, 13, A0);
 
 int funkyTown[] = {262, 330, 392, 262, 392, 330, 262};
 int botaoNotas[] = {262, 330, 392, 523};
+
+void tocarNota(int nota) {
+    tone(BUZZER, nota, 300);
+    delay(400);
+    noTone(BUZZER);
+}
