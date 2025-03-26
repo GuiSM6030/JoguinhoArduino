f625de914207b81705de85974fd4cd1687c08326
Natashinha98 <147116370+Natashinha98@users.noreply.github.com>
2025-03-26 11:43:51 -0300

    Configuração inicial de hardware e pinos

--- primeira.cpp
+++ primeira.cpp
@@ -1,20 +1,23 @@
+#include <LiquidCrystal.h>
 
+// Definição dos pinos
 #define BUZZER 8
 #define LED_VERDE 6
 #define LED_VERMELHO 7
 
+// Botões com pull-up interno
 #define BOTAO1 2
 #define BOTAO2 3
 #define BOTAO3 4
 #define BOTAO4 5
 
-LiquidCrystal lcd(9, 11, 10, 12, 13, A0);
+void setup() {
+    pinMode(BUZZER, OUTPUT);
+    pinMode(LED_VERDE, OUTPUT);
+    pinMode(LED_VERMELHO, OUTPUT);
 
-int funkyTown[] = {262, 330, 392, 262, 392, 330, 262};
-int botaoNotas[] = {262, 330, 392, 523};
-
-void tocarNota(int nota) {
-    tone(BUZZER, nota, 300);
-    delay(400);
-    noTone(BUZZER);
+    pinMode(BOTAO1, INPUT_PULLUP);
+    pinMode(BOTAO2, INPUT_PULLUP);
+    pinMode(BOTAO3, INPUT_PULLUP);
+    pinMode(BOTAO4, INPUT_PULLUP);
 }
