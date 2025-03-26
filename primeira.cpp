6aaa87de66ebb3125934722a2ba8cd600030d912
Natashinha98 <147116370+Natashinha98@users.noreply.github.com>
2025-03-26 11:50:48 -0300

    Verifica se o jogador acertou a sequência

--- primeira.cpp
+++ primeira.cpp
@@ -11,6 +11,12 @@
 #define BOTAO3 4
 #define BOTAO4 5
 
+// Array com a sequência da música "Funky Town"
+int funkyTown[] = {262, 330, 392, 262, 392, 330, 262};
+// Notas correspondentes a cada botão
+int botaoNotas[] = {262, 330, 392, 523};
+LiquidCrystal lcd(9, 11, 10, 12, 13, A0);
+
 void setup() {
     pinMode(BUZZER, OUTPUT);
     pinMode(LED_VERDE, OUTPUT);
@@ -27,16 +33,59 @@ void setup() {
     lcd.print("botao para comecar");
 }
 
-// Array com a sequência da música "Funky Town"
-int funkyTown[] = {262, 330, 392, 262, 392, 330, 262};
-// Notas correspondentes a cada botão
-int botaoNotas[] = {262, 330, 392, 523};
-LiquidCrystal lcd(9, 11, 10, 12, 13, A0);
-
 void tocarNota(int nota) {
     tone(BUZZER, nota, 300);
     delay(400);              
     noTone(BUZZER);
+}
+
+bool verificarSequencia(int nivel) {
+    for (int i = 0; i < nivel; i++) {
+        bool notaCorreta = false;
+        
+        while (!notaCorreta) {
+            if (digitalRead(BOTAO1) == LOW) {
+                tocarNota(botaoNotas[0]);
+                if (botaoNotas[0] == funkyTown[i]) {
+                    notaCorreta = true;
+                } else {
+                    return false;
+                }
+                while (digitalRead(BOTAO1) == LOW); 
+            }
+            
+            if (digitalRead(BOTAO2) == LOW) {
+                tocarNota(botaoNotas[1]);
+                if (botaoNotas[1] == funkyTown[i]) {
+                    notaCorreta = true;
+                } else {
+                    return false;
+                }
+                while (digitalRead(BOTAO2) == LOW);
+            }
+            
+            if (digitalRead(BOTAO3) == LOW) {
+                tocarNota(botaoNotas[2]);
+                if (botaoNotas[2] == funkyTown[i]) {
+                    notaCorreta = true;
+                } else {
+                    return false;
+                }
+                while (digitalRead(BOTAO3) == LOW);
+            }
+            
+            if (digitalRead(BOTAO4) == LOW) {
+                tocarNota(botaoNotas[3]);
+                if (botaoNotas[3] == funkyTown[i]) {
+                    notaCorreta = true;
+                } else {
+                    return false;
+                }
+                while (digitalRead(BOTAO4) == LOW);
+            }
+        }
+    }
+    return true;
 }
 
 
