d85b8be1b09e6bf17a505e7e1a430956d8cc010d
Natashinha98 <147116370+Natashinha98@users.noreply.github.com>
2025-04-02 09:49:10 -0300

    2 musicas adicionadas//redefinição de notas padrões

--- primeira.cpp
+++ primeira.cpp
@@ -1,23 +1,37 @@
 #include <LiquidCrystal.h>
 
-// Definição dos pinos
 #define BUZZER 8
 #define LED_VERDE 6
 #define LED_VERMELHO 7
 #define LED_BOTAO1 A1
 #define LED_BOTAO2 A2
 
-// Botões com pull-up interno
 #define BOTAO1 2
 #define BOTAO2 3
 #define BOTAO3 4
 #define BOTAO4 5
 
-// Array com a sequência da música "Funky Town"
-int funkyTown[] = {262, 330, 392, 262, 392, 330, 262};
-// Notas correspondentes a cada botão
-int botaoNotas[] = {262, 330, 392, 523};
 LiquidCrystal lcd(9, 11, 10, 12, 13, A0);
+
+#define NOTA_E4  330  // Mi (Botão 1 - Verde)
+#define NOTA_A4  440  // Lá (Botão 2 - Vermelho)
+#define NOTA_Cs5 554  // Dó# (Botão 3 - Amarelo)
+#define NOTA_E5  659  // Mi (Botão 4 - Azul)
+
+int jingleBell[] = {NOTA_E4, NOTA_A4, NOTA_Cs5, NOTA_E5, NOTA_A4, NOTA_E4};
+int jingleDurations[] = {4, 4, 2, 4, 4, 2};
+
+int funkyTown[] = {NOTA_E4, NOTA_E4, NOTA_E4, NOTA_A4, NOTA_Cs5, NOTA_Cs5, NOTA_Cs5, NOTA_A4};
+int funkyDurations[] = {4, 4, 4, 4, 4, 4, 4, 4};
+
+int gonnaFlyNow[] = {NOTA_E4, NOTA_A4, NOTA_Cs5, NOTA_Cs5, NOTA_E5, NOTA_E4};
+int gonnaFlyDurations[] = {4, 4, 4, 4, 2, 2};
+
+int happyBirthday[] = {NOTA_E4, NOTA_E4, NOTA_A4, NOTA_E4, NOTA_Cs5, NOTA_A4, NOTA_E4, NOTA_E4, NOTA_A4, NOTA_E4, NOTA_E5, NOTA_Cs5};
+int happyBirthdayDurations[] = {4, 8, 4, 4, 4, 2, 4, 8, 4, 4, 4, 2};
+
+int myGirl[] = {NOTA_E4, NOTA_A4, NOTA_E4, NOTA_Cs5, NOTA_E5, NOTA_Cs5, NOTA_A4, NOTA_A4, NOTA_E4, NOTA_E4, NOTA_A4, NOTA_E4};
+int myGirlDurations[] = {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
 
 void setup() {
     pinMode(BUZZER, OUTPUT);
