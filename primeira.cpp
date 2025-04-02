b8f50c1b72b8455162a5a08de6d71eaf79f98156
Natashinha98 <147116370+Natashinha98@users.noreply.github.com>
2025-04-02 09:51:58 -0300

    Armazenamento das musicas

--- primeira.cpp
+++ primeira.cpp
@@ -33,131 +33,25 @@ int happyBirthdayDurations[] = {4, 8, 4, 4, 4, 2, 4, 8, 4, 4, 4, 2};
 int myGirl[] = {NOTA_E4, NOTA_A4, NOTA_E4, NOTA_Cs5, NOTA_E5, NOTA_Cs5, NOTA_A4, NOTA_A4, NOTA_E4, NOTA_E4, NOTA_A4, NOTA_E4};
 int myGirlDurations[] = {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
 
-void setup() {
-    pinMode(BUZZER, OUTPUT);
-    pinMode(LED_VERDE, OUTPUT);
-    pinMode(LED_VERMELHO, OUTPUT);
+struct Musica {
+  const char* nome;
+  int* notas;
+  int* duracoes;
+  int tamanho;
+  int niveis;
+};
 
-    pinMode(BOTAO1, INPUT_PULLUP);
-    pinMode(BOTAO2, INPUT_PULLUP);
-    pinMode(BOTAO3, INPUT_PULLUP);
-    pinMode(BOTAO4, INPUT_PULLUP);
+Musica musicas[] = {
+  {"Jingle Bell", jingleBell, jingleDurations, sizeof(jingleBell)/sizeof(int), 5},
+  {"Funky Town", funkyTown, funkyDurations, sizeof(funkyTown)/sizeof(int), 4},
+  {"Rocky Theme", gonnaFlyNow, gonnaFlyDurations, sizeof(gonnaFlyNow)/sizeof(int), 4},
+  {"Happy Birthday", happyBirthday, happyBirthdayDurations, sizeof(happyBirthday)/sizeof(int), 5},
+  {"My Girl", myGirl, myGirlDurations, sizeof(myGirl)/sizeof(int), 4}
+};
 
-    lcd.begin(16, 2);
-    lcd.print("Pressione um");
-    lcd.setCursor(0, 1);
-    lcd.print("botao para comecar");
-}
-
-void tocarNota(int nota) {
-    tone(BUZZER, nota, 300);
-    delay(400);              
-    noTone(BUZZER);
-}
-
-bool verificarSequencia(int nivel) {
-    for (int i = 0; i < nivel; i++) {
-        bool notaCorreta = false;
-        
-        while (!notaCorreta) {
-            if (digitalRead(BOTAO1) == LOW) {
-                tocarNota(botaoNotas[0]);
-                if (botaoNotas[0] == funkyTown[i]) {
-                    notaCorreta = true;
-                } else {
-                    return false;
-                }
-                while (digitalRead(BOTAO1) == LOW); 
-            }
-            
-            if (digitalRead(BOTAO2) == LOW) {
-                tocarNota(botaoNotas[1]);
-                if (botaoNotas[1] == funkyTown[i]) {
-                    notaCorreta = true;
-                } else {
-                    return false;
-                }
-                while (digitalRead(BOTAO2) == LOW);
-            }
-            
-            if (digitalRead(BOTAO3) == LOW) {
-                tocarNota(botaoNotas[2]);
-                if (botaoNotas[2] == funkyTown[i]) {
-                    notaCorreta = true;
-                } else {
-                    return false;
-                }
-                while (digitalRead(BOTAO3) == LOW);
-            }
-            
-            if (digitalRead(BOTAO4) == LOW) {
-                tocarNota(botaoNotas[3]);
-                if (botaoNotas[3] == funkyTown[i]) {
-                    notaCorreta = true;
-                } else {
-                    return false;
-                }
-                while (digitalRead(BOTAO4) == LOW);
-            }
-        }
-    }
-    return true;
-}
-
-void loop() {
-    // Inicia o jogo ao pressionar qualquer botão
-    if (digitalRead(BOTAO1) == LOW || digitalRead(BOTAO2) == LOW || 
-        digitalRead(BOTAO3) == LOW || digitalRead(BOTAO4) == LOW) {
-        
-        int nivel = 3; // Nível inicial
-        bool ganhou = true;
-
-        while (ganhou && nivel <= 7) {
-            // Mostra o nível atual
-            lcd.clear();
-            lcd.print("Nivel: ");
-            lcd.print(nivel);
-            delay(1000);
-
-            // Toca a sequência para memorização
-            for (int i = 0; i < nivel; i++) {
-                tocarNota(funkyTown[i]);
-                delay(500);
-            }
-
-            lcd.clear();
-            lcd.print("Sua vez!");
-
-            // Verifica a resposta do jogador
-            if (!verificarSequencia(nivel)) {
-                ganhou = false;
-                lcd.clear();
-                lcd.print("Errou! Fim do jogo");
-                digitalWrite(LED_VERMELHO, HIGH);
-                delay(2000);
-                digitalWrite(LED_VERMELHO, LOW);
-            } else {
-                lcd.clear();
-                lcd.print("Correto!");
-                digitalWrite(LED_VERDE, HIGH);
-                delay(1000);
-                digitalWrite(LED_VERDE, LOW);
-                nivel++;
-            }
-        }
-
-        // Mensagem de vitória
-        if (ganhou) {
-            lcd.clear();
-            lcd.print("Parabens! Venceu!");
-            delay(2000);
-        }
-        
-        // Reinicia o jogo
-        lcd.clear();
-        lcd.print("Pressione um");
-        lcd.setCursor(0, 1);
-        lcd.print("botao para jogar");
-    }
-}
-
+const int NUM_MUSICAS = sizeof(musicas)/sizeof(Musica);
+int musicaSelecionada = 0;
+int nivelAtual = 1;
+int sequencia[12]; 
+int indiceSequencia = 0;
+bool emJogo = false;
